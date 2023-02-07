45aa1df462f074a5ec296dcd28199cae8280019d
Quin Schurman <schurmanq@gmail.com>
2023-02-06 20:27:16 -0500

    Code cleanup

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,23 @@
+--
+--        RedEM-RP 2023 
+-- https://github.com/RedEM-RP
+--
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+
+shared_scripts {
+    'config.lua'
+}
+
 client_scripts {
-    'client/config.lua',
-    'client/client.lua'
+    'client/main.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server.lua'
+    'server/main.lua'
 }
 
 ui_page('client/html/UI.html') 
@@ -13,14 +25,10 @@ ui_page('client/html/UI.html')
 
 files {
 	'client/html/UI.html', 
+    'client/html/script.js',
     'client/html/style.css',
     'client/html/media/font/crock.ttf',
     'client/html/media/img/bg.png',
 	'client/html/media/img/bg2.png',
     'client/html/media/img/bgt.png',
 }
-
-game 'rdr3'
-
-fx_version 'adamant'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
