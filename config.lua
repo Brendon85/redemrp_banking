c124efb17a856300ab5c4d7e77ebf451e7f3bb95
BrendonSmith <81374445+Brendon85@users.noreply.github.com>
2023-02-07 21:39:59 +0800

    Update config.lua
    add blip

--- config.lua
+++ config.lua
@@ -26,3 +26,18 @@ Config.BankLocations = {
         Position = vector3(2994.4484, 571.15026, 44.333209),
     },
 }
+
+Config.BlipName = 'Bank'
+Config.BlipSprite = 1838354131 -- Clothing shop sprite
+Config.BlipScale = 0.2 -- Blip scale
+
+Config.Zones = {
+	vector3(-308.3742, 775.88073, 118.7517), -- VALENTINE
+	--vector3(-304.88,775.19,118.7), -- VALENTINE
+	vector3(1293.299, -1302.006, 77.090515), -- RHODES
+	--vector3(1294.25,-1303.24,77.04), -- RHODES
+	vector3(2644.0393, -1292.54, 52.299583), -- SAINT DENIS
+	vector3(-811.8597, -1277.826, 43.687698), -- BLACK WATER
+	vector3(-3665.388, -2626.68, -13.53777), -- ARMADILO
+
+}
